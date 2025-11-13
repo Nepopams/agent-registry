be05a685b162a77c4aba73796643cfd33e499dcd
vrostov <vrostov@systematica-consulting.ru>
2025-11-13 09:47:56 +0300

    AAR-10 [S1.E1.T5] Storage: ensureIndexes скрипт

---
+++ scripts/ensureIndexes.ts
@@ -0,0 +1,9 @@
+﻿import { getDb, closeDb } from "../src/db/mongo";
+import { ensureIndexes } from "../src/db/agentsRepo";
+
+const run = async () => {
+  const db = await getDb();
+  await ensureIndexes(db);
+  await closeDb();
+};
+run().catch(e => { console.error(e); process.exit(1); });
